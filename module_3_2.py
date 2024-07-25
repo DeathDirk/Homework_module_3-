86532b6faf1fefb8b57db8097d81c522e0c2d8f9
DeathDirk <dirkoflife@gmail.com>
2024-07-25 18:59:44 +0300

    1

--- module_3_2.py
+++ module_3_2.py
@@ -7,7 +7,7 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
         print("Нельзя отправить письмо самому себе!")
 
     elif sender == 'university.help@gmail.com':
-        print(F"Письмо успешно отправлено с  адреса {sender} на адрес {recipient}.")
+        print(f"Письмо успешно отправлено с  адреса {sender} на адрес {recipient}.")
 
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
