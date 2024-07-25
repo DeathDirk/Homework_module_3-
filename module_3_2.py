d3b1b08ff00041a429c951bf202def9aed1c7f9b
DeathDirk <dirkoflife@gmail.com>
2024-07-25 18:44:24 +0300

    1

--- module_3_2.py
+++ module_3_2.py
@@ -9,7 +9,7 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
     elif sender == 'university.help@gmail.com':
         print(F"Письмо успешно отправлено с  адреса {sender} на адрес {recipient}.")
 
-    else :
+    else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
