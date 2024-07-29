3001ba1c60c1f8ac69b88aae17721d3f652026ad
DeathDirk <dirkoflife@gmail.com>
2024-07-29 21:55:41 +0300

    1

--- module_3_2.py
+++ module_3_2.py
@@ -1,23 +1,23 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    #error_v = ["@", ".com", ".ru", ".net"]
-    # if('@' and (".com" or ".ru" or ".net")) not in (recipient and sender):
-    #       print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    if ('@' or ".com" or ".ru" or ".net") not in recipient or ('@' or ".com" or ".ru" or ".net") not in sender:
+    if("@" not in recipient or '@' not in sender) or (not recipient.endswith((".com", '.ru', 'net'))
+                                                       and not sender.endswith(('.com', '.ru', '.net'))):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+        return
 
-    elif sender == recipient:
+    if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
+        return
 
-    elif sender == 'university.help@gmail.com':
+    if sender == 'university.help@gmail.com':
         print(f"Письмо успешно отправлено с  адреса {sender} на адрес {recipient}.")
-
-    elif sender != 'university.help@gmail.com':
+    else:
+    # elif sender != 'university.help@gmail.com':
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.uk', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
 #Пункты задачи:
 # Создайте функцию send_email, которая принимает 2 обычных аргумента: message(сообщение), recipient(получатель) и 1
@@ -36,3 +36,14 @@ send_email('Напоминаю самому себе о вебинаре', 'urba
 # НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
 # Невозможно отправить письмо с адреса urban.teacher@mail.uk на адрес urban.student@mail.ru
 # Нельзя отправить письмо самому себе!
+
+# if ('@' or (".com" or ".ru" or ".net")) not in (recipient or sender):
+#     print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+#     return
+
+#error_v = ["@", ".com", ".ru", ".net"]
+    # if('@' and (".com" or ".ru" or ".net")) not in (recipient and sender):
+    #       print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    # if ('@' or ".com" or ".ru" or ".net") not in recipient or ('@' or ".com" or ".ru" or ".net") not in sender:
+    #     print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+    #     return
